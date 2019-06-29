fe16011ceb352729bb6a3d39dcae963792101484
Veeral Patel <vpatel98@gmail.com>
2019-06-29 15:22:00 -0700

    remove edit route (part 2)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :scrapes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :scrapes, except: [:edit]
+  
+  root :to => 'scrapes#index'
 end
